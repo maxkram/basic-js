522a27146aacbf7d9274223176638f7b66b57d28
maxkram <max.kramer@live.com>
2022-04-30 13:53:41 -0400

    i can solve minesweeper and vigenere

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -20,57 +20,15 @@ const { NotImplementedError } = require("../extensions/index.js");
  *
  */
 class VigenereCipheringMachine {
-  constructor(direct = true) {
-    this.direct = direct;
+  encrypt() {
+    throw new NotImplementedError("Not implemented");
+    // remove line with error and write your code here
   }
-
-  encrypt(str, key) {
-    return this.Algo(str, key, "encrypt");
-  }
-  decrypt(str, key) {
-    return this.Algo(str, key, "decrypt");
-  }
-
-  Algo(str, key, mode) {
-    if (!str || !key || !mode) throw Error("Incorrect arguments!");
-
-    str = str.toUpperCase();
-    key = key.toUpperCase();
-    let res = [];
-
-    for (let i = 0, j = 0; i < str.length; i++) {
-      if (str[i].match(/[A-Z]/)) {
-        if (mode == "decrypt") {
-          res.push(
-            String.fromCharCode(
-              ((str[i].charCodeAt(0) -
-                "A".charCodeAt(0) +
-                (26 -
-                  (key[j % key.length].charCodeAt(0) - "A".charCodeAt(0)))) %
-                26) +
-                "A".charCodeAt(0)
-            )
-          );
-        } else {
-          res.push(
-            String.fromCharCode(
-              ((str[i].charCodeAt(0) -
-                "A".charCodeAt(0) +
-                (key[j % key.length].charCodeAt(0) - "A".charCodeAt(0))) %
-                26) +
-                "A".charCodeAt(0)
-            )
-          );
-        }
-        j++;
-      } else {
-        res.push(str[i]);
-      }
-    }
-    return this.direct ? res.join("") : res.reverse().join("");
+  decrypt() {
+    throw new NotImplementedError("Not implemented");
+    // remove line with error and write your code here
   }
 }
-// console.log(Alog());
 
 module.exports = {
   VigenereCipheringMachine,
